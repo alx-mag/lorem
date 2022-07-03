2f10192bd238f1177125c128b60d6ec0b6102012
alx.mag <alx.serg.mag@gmail.com>
2022-07-03 14:33:04 +0400

    Remove dead code

--- src/main/kotlin/com/github/alxmag/loremipsumgenerator/model/LoremModel.kt
+++
@@ -1,8 +0,0 @@
-package com.github.alxmag.loremipsumgenerator.model
-
-import com.github.alxmag.loremipsumgenerator.util.TextAmountUnit
-
-interface LoremModel {
-    var amount: Int
-    var unit: TextAmountUnit
-}
