f803a19fdf4f1acf899d8a0dc0917c45e86557f6
alx.mag <alx.serg.mag@gmail.com>
2022-07-18 21:30:20 +0400

    Street Address Action

--- src/main/kotlin/com/github/alxmag/loremipsumgenerator/action/address/actions.kt
+++ src/main/kotlin/com/github/alxmag/loremipsumgenerator/action/address/actions.kt
@@ -1,10 +1,15 @@
 package com.github.alxmag.loremipsumgenerator.action.address
 
+import com.github.alxmag.loremipsumgenerator.MyBundle.message
 import com.github.alxmag.loremipsumgenerator.action.base.LoremEditorAction
 import com.github.alxmag.loremipsumgenerator.action.base.LoremTextFactoryActonHandler.Companion.simpleFakerHandler
 
 class LoremCityAction : LoremEditorAction(simpleFakerHandler { it.address().city() })
 class LoremCountryAction : LoremEditorAction(simpleFakerHandler { it.address().country() })
 class LoremStateAction : LoremEditorAction(simpleFakerHandler { it.address().state() })
+
 // TODO register
-class LoremStreetAction : LoremEditorAction(simpleFakerHandler { it.address().streetAddress() })
+class LoremStreetAction : LoremEditorAction(
+    simpleFakerHandler { it.address().streetAddress() },
+    message("action.street.address")
+)
--- src/main/kotlin/com/github/alxmag/loremipsumgenerator/action/base/LoremEditorAction.kt
+++ src/main/kotlin/com/github/alxmag/loremipsumgenerator/action/base/LoremEditorAction.kt
@@ -1,7 +1,21 @@
 package com.github.alxmag.loremipsumgenerator.action.base
 
 import com.github.alxmag.loremipsumgenerator.action.recent.LoremMemorizeAction
+import com.github.alxmag.loremipsumgenerator.util.LoremActionPlace
 import com.intellij.openapi.editor.actionSystem.EditorAction
 import com.intellij.openapi.editor.actionSystem.EditorActionHandler
+import org.jetbrains.annotations.Nls
 
-abstract class LoremEditorAction(defaultHandler: EditorActionHandler) : EditorAction(defaultHandler), LoremMemorizeAction
+abstract class LoremEditorAction(
+    defaultHandler: EditorActionHandler,
+    @Nls(capitalization = Nls.Capitalization.Title) baseName: String? = null
+) : EditorAction(defaultHandler), LoremMemorizeAction {
+    init {
+        if (baseName != null) {
+            addTextOverride(LoremActionPlace.EDITOR_POPUP, baseName)
+            templatePresentation.text = "Lorem: $baseName"
+        }
+    }
+
+    final override fun addTextOverride(place: String, text: String) = super.addTextOverride(place, text)
+}
